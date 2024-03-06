5db32be3358fd5090595f15abda8597bfb3e192e
mgalihpp <muhammadgalih451@gmail.com>
2024-03-06 13:07:21 +0700

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,23 @@
-export default function middleware() {}
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.has("next-auth.session-token");
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/?callback=not_authorized", req.url));
+  }
+
+  if (req.nextUrl.pathname === "/organization") {
+    return NextResponse.redirect(new URL("/select-organization", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Configuration for the middleware
+export const config = {
+  matcher: [
+    // Exclude specific paths, allowing all others
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|logo.svg|public/).+)",
+  ],
+};
