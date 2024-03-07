c6400ad423657ab2b4912799e737abcd26c68ccd
mgalihpp <muhammadgalih451@gmail.com>
2024-03-07 15:56:36 +0700

    add: cache to image, types

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: '*'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+      },
+    ],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
